4ca991039ceb9ea4dc8c36ea21ff2ee73a442423
JuanGabriel2960 <58345059+JuanGabriel2960@users.noreply.github.com>
2020-06-12 15:05:01 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,146 +1,127 @@
 #Import the classes stored in the different modules of the main package.
-from classes.player import *
-from classes.money import *
-from classes.cpu import *
-from classes.card import *
+from classes.greeting import Greeting
+from classes.pack import Pack
+from classes.play import Play
+from classes.money import Money
 
-#Import python's own modules to generate randomness and clean screen.
-import random
+#Import python own modules to clean screen and add delay for each play.
 import time
 import os
 
 os.system("cls")
 
-#The "pack" class combines the value of the cards and their form and groups them in the "pack" arrangement.
-class pack:
-	def __init__(self):
-		self.pack = []
-		for shapes_of_the_cards in ['♥','♦','♣','♠']:
-			for values_of_the_cards in range(2,11):
-				self.pack.append(card(shapes_of_the_cards,values_of_the_cards))
-			for values_of_the_cards in ["J","Q","K","A"]:
-				self.pack.append(card(shapes_of_the_cards,values_of_the_cards))
-		random.shuffle(self.pack)
-#The show_pack method, traces each element (card) of the "pack" arrangement.
-	def show_pack(self):
-		for accountant in self.pack:
-			print(str(accountant.values_of_the_cards) + accountant.shapes_of_the_cards)
-#The "give_one_card" method give a card to the player and proceed to remove it from the pack.
-	def give_one_card(self, player_1):
-		player_1.receive_card(self.pack[0])
-		self.pack.remove(hand.pack[0])
+player_name = str(input('Enter player name: '))
+os.system("cls")
 
-#Instance "Chip" belonging to the "Money" class, which handles the amount of money wagered.
-Chip = Money()
+#Create the 'player' object belonging to the 'Greeting' class through which it will get the name by parameter.
+player = Greeting(player_name)
+player.greet()
 
-while(Chip.money > 0 and Chip.money < 5000 ):
+print('1-Yes, I want to read the rules of the game')
+print('2-No, I know how to play\n')
+read_rules = int(input())
 
-	print("\n$1 min - $5,000 max / To keep playing at the table")
-	print("Current money: $",Chip.money)
+player.read(read_rules)
+next=(input('Press enter to continue: '))
+os.system("cls")
 
-	#Build the "hand" object of the "pack" class.
-	hand = pack()
+chip = Money()
 
-	print("------------------------------------------------------------------------------------------------------------------")
-	print("\nCroupier cards\n")
+#The basic rules are established to continue playing that are nothing more than complying with that interval of coins.
+while(chip.money > 0 and chip.money < 5000 ):
 
-	#Build the first player and proceed to deliver 2 cards.
-	Croupier = CPU()
-	hand.give_one_card(Croupier)
+	print('\n$1 min - $5,000 max / To keep playing at the table')
+	print('Current money: $',chip.money)
 
-	#The 2 cards are shown.
-	Croupier.show_find()
+	print('------------------------------------------------------------------------------------------------------------------------')
+	print('\nCroupier cards\n')
 
+	#Build the 'CPU' player and proceed to deliver 1 cards.
+	CPU = Play()
+	CPU.deliver_card()
+	
 	#The user is asked how much they want to bet.
 	while(True):
 		try:
-			player_bet=int(input("\nHow much do you want to bet?: "))
+			player_bet=int(input('\nHow much do you want to bet?: '))
 			if type(player_bet) != int:
 				raise ValueError
-			if player_bet < 1 or player_bet > Chip.money:
+			if player_bet < 1 or player_bet > chip.money:
 				raise TypeError 
 			break
 		except (ValueError):
-			print("Enter a numeric value")
+			print('Enter a numeric value (●__●)')
 		except (TypeError):
 			if player_bet < 1:
-				print("Enter a positive value")
-			if player_bet > Chip.money:
-				print("Enter a lower bet")
+				print('Enter a positive value (●__●)')
+			if player_bet > chip.money:
+				print('Enter a lower bet (●__●)')
 
-	print("Remaining money: $",Chip.money - player_bet)
+	print('Remaining money: $',chip.money - player_bet)
 
-	print("\nPlayer cards\n")
+	print('\nPlayer cards\n')
 
-	#Build the first player and proceed to deliver 2 cards.
-	player_1 = player()
-	hand.give_one_card(player_1)
-	hand.give_one_card(player_1)
-
-	#The 2 cards are shown.
-	player_1.show_find()
+	#Build the 'player' and proceed to deliver 2 cards.
+	player = Play()
+	player.deliver_card()
+	player.deliver_card()
 
 	#The player is asked to ask for request a card or stand.
-	while(player_1.count_find() < 21):
-		
+	while(player.count_card() < 21):
+		#Validated by 'try' that the player's answer is correct.
 		while (True):
 			try:
-				print("\nWhat would you like to do?")
-				print("1-Request a card")
-				print("2-Stand")
-				player_answer = int(input())
-				print(" ")
+				print('What would you like to do?')
+				player_answer = int(input('1-Request a card / 2-Stand: '))
+				print('\n')
 				if type(player_answer) != int:
 					raise ValueError
 				if player_answer < 1 or player_answer > 2:
 					raise TypeError 
 				break
 			except (ValueError):
-				print("Enter a numeric value")
+				print('\nEnter a numeric value ಠ_ಠ\n')
 			except (TypeError):
-				print("Enter a value between 1 or 2")
+				print('\nEnter a value between 1 or 2 ಠ_ಠ\n')
 
 		#If the answer is 1, another card is given to the player.
 		if(player_answer == 1):
 			time.sleep(1)
-			hand.give_one_card(player_1)
-			player_1.show_find()
+			player.deliver_card()
 		#If the answer is 2, the player stands and the remaining cards are given to the Croupier.
 		if(player_answer == 2):
 			time.sleep(2)
-			hand.give_one_card(Croupier)
-			Croupier.show_find()
+			CPU.deliver_card()
 			#As long as the dealer's cards total less than 16, he must ask for another.
-			while (Croupier.count_find()<=16):
+			while (CPU.count_card()<=16):
 				time.sleep(2)
-				hand.give_one_card(Croupier)
-				Croupier.show_find()
+				CPU.deliver_card()
 			#If the dealer's cards are greater than the player's, but at the same time less than or equal to 21, he wins.
-			if Croupier.count_find() > player_1.count_find() and Croupier.count_find() <= 21:
-				print("\nDealer wins")
-			#The "bet_money" function is called with the Boolean parameter 0 to indicate that the player lost.
-				Chip.bet_money(player_bet,False)
+			if CPU.count_card() > player.count_card() and CPU.count_card() <= 21:
+				print('\nDealer wins (ᴗ˳ᴗ)')
+			#The "bet_money" function is called with the Boolean parameter 'False' to indicate that the player lost.
+				chip.bet_money(player_bet,False)
 				break
 			else:
-			#The "bet_money" function is called with the Boolean parameter 1 to indicate that the player won,
-				print("\nPlayer wins")
-				Chip.bet_money(player_bet,True)
+			#The "bet_money" function is called with the Boolean parameter 'True' to indicate that the player won.
+				print('\nPlayer wins (ง^ᗜ^)ง')
+				chip.bet_money(player_bet,True)
 				break
 	#If the player does not get to stand at any time and automatically goes over 21, the dealer wins.
-	if (player_1.count_find() > 21):
-		print("\nDealer wins")
-		Chip.bet_money(player_bet,False)
+	if (player.count_card() > 21):
+		print('\nDealer wins (ᴗ˳ᴗ)')
+		chip.bet_money(player_bet,False)
 	#If the player does not get to stand at any time, but adds 21 he has blackjack and automatically wins.
-	elif (player_1.count_find() == 21):
-		print("\nBlackjack! The player wins")
-		Chip.bet_money(player_bet,True)
+	elif (player.count_card() == 21):
+		print('\nBlackjack! The player wins (ง^ᗜ^)ง')
+		chip.bet_money(player_bet,True)
 
-	next_clear_console=(input("\nPress enter to continue: "))
+	next_clear_console=(input('\nPress enter to continue: '))
 	os.system("cls")
 		
 #If the player's chips run out, he must leave the table and end the game.
-if (Chip.money < 1):
-	print("\nInsufficient chip, must leave the table")
+if (chip.money < 1):
+	print('\nInsufficient chip, must leave the table (╥_╥)')
 else:
 #If the player's coins exceed the maximum allowed, he must leave the table and end the game.
-	print("\nMaximum chip for this table")
+	print('\nMaximum chip for this table, you win ฅ^•ﻌ•^ฅ')
